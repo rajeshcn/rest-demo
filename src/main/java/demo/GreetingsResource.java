8cbc087067ca3a9d4c7f5ab6b3b9561b8d62f297
rajeshcn <rajeshcn@gmail.com>
2015-03-06 17:46:30 +0530

    removed GET commmand

--- src/main/java/demo/GreetingsResource.java
+++ src/main/java/demo/GreetingsResource.java
@@ -1,7 +1,5 @@
 package demo;
 
-import javax.ws.rs.GET;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingsResource {
 	
-	@GET
 	@RequestMapping("/greetings/{name}")
 	public String sayHello(@PathVariable String name) {
 		return "hello rest app world "+name;
